c05d9d334439173681ea17b6fd0549cc9fa3f8f4
MahmudQasem <60925740+MahmudQasem@users.noreply.github.com>
2021-01-12 10:14:02 +0200

    Update bla.php

--- bla.php
+++ bla.php
@@ -1,19 +1,5 @@
-$ch = curl_init();
- 
-//Set the URL that you want to GET by using the CURLOPT_URL option.
-curl_setopt($ch, CURLOPT_URL, 'https://1e492660cbf23ce4ba61a5fb128445b9.m.pipedream.net');
- 
-//Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- 
-//Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
- 
-//Execute the request.
-$data = curl_exec($ch);
- 
-//Close the cURL handle.
-curl_close($ch);
-
-//Print the data out onto the page.
-echo $data;
+<?php
+$r = new HttpRequest('https://1e492660cbf23ce4ba61a5fb128445b9.m.pipedream.net', HttpRequest::METH_GET);
+$r->send();
+echo $r->getResponseCode();
+?>
